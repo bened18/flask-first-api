b4359c6223fe5dc21da59e931187baf3cbd044a6
bened18 <37964425+bened18@users.noreply.github.com>
2023-10-31 07:07:00 -0500

    factorial flask api

---
+++ factorial_service.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+@app.route('/factorial/<int:number>', methods=['GET'])
+def get_factorial(number):
+    if number < 0:
+        return jsonify(error="Número negativo no permitido"), 400
+    else:
+        result = factorial(number)
+        return jsonify(factorial=result)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
